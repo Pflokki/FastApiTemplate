fe5fb96d632b3fa9ea0b0b8762680d93eba022fc
Konstant.Uvarov <konstant.uvarov@x5.ru>
2022-07-28 03:38:03 +0400

    add supporting sqlalchemy and asyncpg, create RWDatabase class for connecting to db, create test db docker instance

--- src/settings/common.py
+++ src/settings/common.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from pydantic.env_settings import BaseSettings
 from enum import Enum
 
@@ -14,6 +15,13 @@ class CommonSettings(BaseSettings):
     ENVIRONMENT: str = Environment.PRODUCTION.value
     DEBUG: bool = False
     LOG_LEVEL: str = 'INFO'
+
+    PG_SCHEMA_NAME: str = Field(..., env='DB_SCHEMA')
+    PG_RW_DSN: str
+    PG_POOL_SIZE: int = 50
+    PG_POOL_SIZE_MAX_OVERFLOW: int = 10
+    PG_POOL_RECYCLE: int = 1800
+    PG_POOL_TITLE: int = 1
 
     BASE_SERVICE_TIMEOUT: int = 2
     BASE_SERVICE_RETRY_COUNT: int = 3
